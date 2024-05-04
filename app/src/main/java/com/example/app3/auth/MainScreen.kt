40d642ee2372590bc1babe887b626cd493c11ee5
fedelampe11 <federico.lamperti@mail.polimi.it>
2024-05-04 18:20:42 +0200

    Started to do starting page and deleted useless images

--- app/src/main/java/com/example/app3/auth/MainScreen.kt
+++ app/src/main/java/com/example/app3/auth/MainScreen.kt
@@ -2,9 +2,11 @@ package com.example.app3.auth
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -21,38 +23,88 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.app3.DestinationScreen
 import com.example.app3.FbViewModel
 import com.example.app3.R
+import com.example.app3.ui.theme.inter_font
+
+// TODO download and insert background video
+
+val text = buildAnnotatedString {
+    withStyle(style = SpanStyle(color = Color.White)) {
+        append("Never miss your ")
+    }
+    withStyle(style = SpanStyle(color = Color(0xFFFF003D))) {
+        append("stories")
+    }
+}
 
 @Composable
 fun MainScreen(navController: NavController, vm: FbViewModel) {
-    Image(
-        painter = painterResource(id = R.drawable.nike),
-        contentDescription = null,
-        contentScale = ContentScale.Fit,
-        modifier = Modifier.fillMaxSize()
-    )
     Column (
+        horizontalAlignment = Alignment.Start,
+        modifier = Modifier
+            .background(Color.Black)
+            .fillMaxSize(),
+    ) {
+        Text(
+            text = "SERIES\nTIME",
+            style = TextStyle(
+                fontSize = 26.sp,
+                lineHeight = 24.sp,
+                fontFamily = inter_font,
+                fontWeight = FontWeight(700),
+                color = Color.White,
+                ),
+            modifier = Modifier.padding(top = 37.dp, start = 39.dp)
+        )
+
+        Spacer(modifier = Modifier.height(420.dp))
+
+        Text(
+            text = text,
+            style = TextStyle(
+                fontSize = 60.sp,
+                lineHeight = 53.sp,
+                fontFamily = inter_font,
+                fontWeight = FontWeight(1000),
+                color = Color.White,
+            ),
+            modifier = Modifier.padding(start = 37.dp)
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.sharp_arrow_forward_ios_24),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(start = 320.dp)
+                .size(300.dp)
+                .clickable {
+                navController.navigate(DestinationScreen.Login.route)
+            }
+        )
+    }
+
+    /*Column (
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier
             .fillMaxWidth()
             .padding(top = 100.dp)
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.monza),
-            contentDescription = null,
-            Modifier.size(200.dp)
-        )
         Text(
             text = "Welcome Berlusconi!",
-            color = Color.Black,
+            color = Color.White,
             fontWeight = FontWeight.Bold,
             fontSize =  40.sp
         )
@@ -106,5 +158,5 @@ fun MainScreen(navController: NavController, vm: FbViewModel) {
                 )
             }
         }
-    }
+    }*/
 }
--- app/src/main/java/com/example/app3/auth/SuccessScreen.kt
+++ app/src/main/java/com/example/app3/auth/SuccessScreen.kt
@@ -1,12 +1,14 @@
 package com.example.app3.auth
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
 import com.example.app3.FbViewModel
@@ -17,11 +19,10 @@ fun SuccessScreen(navController: NavController, vm: FbViewModel) {
     Column (
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Green)
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.cry),
-            contentDescription = null
-        )
+
     }
 }
