f3aacb223178f404c0a2abfff70a009ff74b97e2
fedelampe11 <federico.lamperti@mail.polimi.it>
2024-05-17 10:52:06 +0200

    Created App logo

--- app/src/main/java/com/example/app3/auth/MainScreen.kt
+++ app/src/main/java/com/example/app3/auth/MainScreen.kt
@@ -3,28 +3,17 @@ package com.example.app3.auth
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Outline
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
@@ -61,7 +50,7 @@ fun MainScreen(navController: NavController, vm: FbViewModel) {
     Column (
         horizontalAlignment = Alignment.Start,
         modifier = Modifier
-            .background(Color.Black)
+            .background(Color(0xFF202027))
             .fillMaxSize(),
     ) {
         Text(
